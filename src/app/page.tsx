3919ede929afc9e92309678f3049b8aee949d95e
Emmanuel <asamoahe304@gmail.com>
2024-08-20 15:06:10 +0000

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button size="lg">Click Me</Button>
+    </main>
+  );
+}
